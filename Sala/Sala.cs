54f8378563165f5e416bc8ab76f6244fb0812a66
Luís Miguel Gonçalves <g.luismiguel14@gmail.com>
2016-04-06 22:40:17 +0100

    Salas GUI updated

--- Sala/Sala.cs
+++ Sala/Sala.cs
@@ -15,22 +15,34 @@ namespace Sala
         private IPedidos listaPedidos;
         private EventIntermediate inter;
         private Panel painelSalas;
-        private Label label1;
-        SortedDictionary<string, double> menuList;
+        private static Label label1;
+        public SortedDictionary<string, double> menuList;
+        public SortedDictionary<int, string> mesasList;
+        public int[] quantityList = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        static void Main()
+        {
+            Application.Run(new Sala());
+        }
 
         public Sala()
         {
             //Inicializar Menu
             menuList = new SortedDictionary<string, double>();
-            menuList.Add("Bacalhau com natas - 7.50€", 7.50);
-            menuList.Add("Francesinha - 9.50€", 9.50);
-            menuList.Add("Cocktail Mexicano - 3.50€", 3.50);
-            menuList.Add("Caipirinha - 3.00€", 3.00);
+            mesasList = new SortedDictionary<int, string>();
+            populateComboMenus();
+
             InitializeComponent();
             comboBoxMenu.DataSource = new BindingSource(menuList, null);
             comboBoxMenu.DisplayMember = "Key";
             comboBoxMenu.ValueMember = "Value";
-            
+            comboBoxMesa.DataSource = new BindingSource(mesasList, null);
+            comboBoxMesa.DisplayMember = "Value";
+            comboBoxMesa.ValueMember = "Key";
+            foreach (int type in quantityList)
+            {
+                comboBoxQuantidade.Items.Add(type);
+            }
         }
 
         private void buttonPedir_Click(object sender, EventArgs e)
@@ -38,9 +50,17 @@ namespace Sala
 
         }
 
-        static void Main()
+        public void populateComboMenus()
         {
-            Application.Run(new Sala());
+            menuList.Add("Bacalhau com natas - 7.50€", 7.50);
+            menuList.Add("Francesinha - 9.50€", 9.50);
+            menuList.Add("Cocktail Mexicano - 3.50€", 3.50);
+            menuList.Add("Caipirinha - 3.00€", 3.00);
+            mesasList.Add(1, "Mesa 1");
+            mesasList.Add(2, "Mesa 2");
+            mesasList.Add(3, "Mesa 3");
+            mesasList.Add(4, "Mesa 4");
+
         }
     }
 }
