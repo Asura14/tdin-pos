eb35c03e86a4a03618e18b1a7c0fab4c94fb8350
Luís Miguel Gonçalves <g.luismiguel14@gmail.com>
2016-04-12 12:28:52 +0100

    Caixa added

---
+++ Caixa/Caixa.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Caixa
+{
+    partial class Caixa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dataGridProntos = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProntos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dataGridProntos
+            // 
+            this.dataGridProntos.AllowUserToAddRows = false;
+            this.dataGridProntos.AllowUserToDeleteRows = false;
+            this.dataGridProntos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridProntos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.descricao,
+            this.estado});
+            this.dataGridProntos.Location = new System.Drawing.Point(12, 69);
+            this.dataGridProntos.Name = "dataGridProntos";
+            this.dataGridProntos.ReadOnly = true;
+            this.dataGridProntos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridProntos.Size = new System.Drawing.Size(344, 350);
+            this.dataGridProntos.TabIndex = 1;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // descricao
+            // 
+            this.descricao.HeaderText = "Descrição";
+            this.descricao.Name = "descricao";
+            this.descricao.ReadOnly = true;
+            // 
+            // estado
+            // 
+            this.estado.HeaderText = "Estado";
+            this.estado.Name = "estado";
+            this.estado.ReadOnly = true;
+            // 
+            // Caixa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 517);
+            this.Controls.Add(this.dataGridProntos);
+            this.Name = "Caixa";
+            this.Text = "Caixa";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProntos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.DataGridView dataGridProntos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn estado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+    }
+}
--- Caixa/Caixa.cs
+++ Caixa/Caixa.cs
@@ -1,31 +1,32 @@
 ﻿using System;
 using System.Collections;
-using System.Runtime.Remoting;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-class Caixa
+namespace Caixa
 {
-    static void Main()
+    public partial class Caixa : Form
     {
-        RemotingConfiguration.Configure("Caixa.exe.config", false);
-        IPedidos pedidos = (IPedidos)RemoteNew.New(typeof(IPedidos));
-
-        EventIntermediate intEvent = new EventIntermediate();
-        Pedido pedido = new Pedido(1, "ok", 3, 1, 1, 10);
-        pedidos.adicionaPedido(pedido);
-        foreach (Pedido ped in pedidos.GetPedidos())
+        public Caixa()
         {
-            Console.WriteLine("ID: " + ped.id + "\n" + "Desc: " + ped.descricao + "\n" + "Estado: " + ped.estado + "\n");
+            InitializeComponent();
         }
-        Console.ReadLine();
 
-
+        static void Main()
+        {
+            Application.Run(new Caixa());
+        }
     }
 
 }
+
 class RemoteNew
 {
     private static Hashtable types = null;
